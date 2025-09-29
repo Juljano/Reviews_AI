2c6aef419a34f97903e528233e2680bcce35142a
Juljano Möller <juljano.moeller@starfinanz.de>
2025-09-29 08:27:16 +0200

    - Model ist fertig
    -- Geplant für die Zukunft:
     - Feintuning

--- ModelExecutor.py
+++ ModelExecutor.py
@@ -1,24 +1,24 @@
 import joblib
 
-
 # load TF-IDF Vectorizer and Logistic Regression
-vectorizer = joblib.load("Model/reviews_vectorizer.pkl")   # TF-IDF Vectorizer
-clf = joblib.load("Model/reviews_tfidf_model.pkl")       # Logistic Regression
+vectorizer = joblib.load("Model/reviews_vectorizer.pkl")  # TF-IDF Vectorizer
+clf = joblib.load("Model/reviews_tfidf_model.pkl")  # Logistic Regression
 
-new_review = ["Ich hasse diese Bank"]
+def starting_model(message_from_user):
 
-#convert review into TF-IDF
-X_new = vectorizer.transform(new_review)
+    new_review = [message_from_user]
+    #convert review into TF-IDF
+    X_new = vectorizer.transform(new_review)
 
-# prediction
-prediction = clf.predict(X_new)
+    # prediction
+    prediction = clf.predict(X_new)
 
-print(F"Prediction: {prediction}")
+    print(F"Prediction: {prediction}")
 
-if prediction <= 2:
-    print("Negative")
-elif prediction >= 4:
-    print("Positive")
-else:
-    print("Neutral")
+    if prediction <= 2:
+        print("Negative")
+    elif prediction >= 4:
+        print("Positive")
+    else:
+        print("Neutral")
 
--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-import Preprocessing
+import os
 
+import Preprocessing
+from ModelExecutor import starting_model
 
 
 if __name__ == '__main__':
-    preprocessor = Preprocessing.Preprocessing()
+
+    if not os.path.exists("Model/reviews_tfidf_model.pkl"):
+        preprocessor = Preprocessing.Preprocessing()
+    else:
+        print("Model existiert schon")
+
+    try:
+        while True:
+            start = starting_model(input("Bitte gebe eine Bewertung ab: "))
+    except KeyboardInterrupt:
+        print("Programm abgebrochen")
+
